6e85075ebb0b4615ada4cdaef515020915f6cab6
Mikhail Krasko <mm.krasko@gmail.com>
2021-12-04 17:36:11 +0300

    0.0.3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 7777
+          Learn React 9999
         </a>
       </header>
     </div>
