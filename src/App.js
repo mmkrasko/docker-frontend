b0694235be5e532d643a38316c58d1de2ff753f1
Mikhail Krasko <mm.krasko@gmail.com>
2021-12-05 12:11:44 +0300

    update learn react

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          learn react
           Change to the feature branch
           and second one
           third one
